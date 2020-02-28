25a712f098308386ffa000ec98049e2a89de4875
Administrator <christopher.cm.mai@gmail.com>
2020-02-27 20:55:48 -0800

    added key summary statistic most recently popular

--- src/components/sub/KeySummaryStatistics.js
+++ src/components/sub/KeySummaryStatistics.js
@@ -1,16 +1,34 @@
 import React from 'react'
-import { Grid } from 'semantic-ui-react'
 
 import './style.css'
 
 export default function KeySummaryStatistics(props) {
+  const labels = props.formattedLabels
+  const datasets = props.formattedData
+
+  function findMostRecentlyPopular() {
+    let mostRecentIdx = labels.length-1
+    let highestPopularity = 0
+    let mostPopularIdx = 0
+    //for each dataset, find the one with the highest data[mostRecentIdx]
+    for (let i = 0; i < datasets.length; i++) {
+      let currentPopularity = datasets[i].data[mostRecentIdx]
+      if (currentPopularity > highestPopularity) {
+        highestPopularity = currentPopularity
+        mostPopularIdx = i
+      }
+    }
+
+    return datasets[mostPopularIdx].label
+  }
 
   return (
     <div>
       <h1>Statistics</h1>
-      //Most/least popular as of most recent data
-      //Rising star in popularity - largest growth
-      //Candidate to watch out for - fastest growth
+      <h3>Most/least popular as of most recent data:</h3>
+      <p>{findMostRecentlyPopular()}</p>
+      <h3>Rising star in popularity - Largest Growth:</h3>
+      <h3>Candidate to watch out for - Fastest Growth:</h3>
     </div>
   )
 }
